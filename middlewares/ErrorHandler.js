9ea88fab44e855f189cf6c21a590bbcfab32eb23
Ekaterina Sokolova <sokoloffkate@gmail.com>
2023-02-08 20:27:28 +1100

    Fixed user routes

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const {
   BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR, FORBIDDEN_ERROR,
-  INTERNAL_SERVER_ERROR_MESSAGE, BAD_REQUEST_MESSAGE,
+  INTERNAL_SERVER_ERROR_MESSAGE,
 } = require('../utils/constants');
 
 module.exports = (err, req, res, next) => {
   if (err instanceof mongoose.Error.CastError || err instanceof mongoose.Error.ValidationError) {
-    return res.status(BAD_REQUEST).send({ message: BAD_REQUEST_MESSAGE });
+    return res.status(BAD_REQUEST).send({ message: err.message });
   }
   if (err.name === 'NotFound') {
     return res.status(NOT_FOUND).send({ message: err.message });
--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,27 @@ const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUser, geCurrenttUser, updateUser, updateAvatar,
 } = require('../controllers/users');
+const { RegUrl } = require('../utils/constants');
 
 UserRouter.get('/', getUsers);
 UserRouter.get('/me', geCurrenttUser);
-UserRouter.get('/:id', getUser);
-UserRouter.patch('/me', updateUser);
-UserRouter.patch('/me/avatar', updateAvatar);
+UserRouter.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().length(24),
+  }),
+}), getUser);
+
+UserRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), updateUser);
+
+UserRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(RegUrl),
+  }),
+}), updateAvatar);
 
 module.exports = UserRouter;
