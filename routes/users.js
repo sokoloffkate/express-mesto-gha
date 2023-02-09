591f29141679986c11d69456943625e628ee180d
Ekaterina Sokolova <sokoloffkate@gmail.com>
2023-02-09 12:42:35 +1100

    added Joi validation to cards

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,8 @@ UserRouter.get('/:id', celebrate({
 
 UserRouter.patch('/me', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 }), updateUser);
 
